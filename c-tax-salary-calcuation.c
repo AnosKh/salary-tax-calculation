ce41eaaec8338b862b93be24546ea9b0716a6b19
Anoskh <kimhab.zeecode@gmail.com>
2020-09-15 00:43:49 +0700

    tax salary calculation khmer (Header)

---
+++ c-tax-salary-calcuation.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   /* my first program in C */
+   printf("ការគណនាពន្ធលើប្រាក់បៀវវត្ស\nHello C Programming. \n");
+   printf("និវាសនជន | អ្នកជាប់ពន្ធជាជាតិដើម / Resident\n");
+   printf("អនិវាសនជន | អ្នកជាប់ពន្ធជាជាតិបរទេស / Non-Resident\n");
+
+   return 0;
+}
